b01c792106615ba574edcc48251dacb1eb7d7a44
Jamie Sammons <jamie.sammons@liferay.com>
2025-04-28 23:11:04 -0500

    gitconfig

--- install.sh
+++ install.sh
@@ -11,4 +11,11 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 rm -rf ~/.config/nvim/.git
 
+PUBLIC_KEY=$(ssh-add -L | grep "Github")
+
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+git config --global user.signingkey "$PUBLIC_KEY"
+git config --global --unset gpg.ssh.program
+
 chsh -s $(which zsh)
