72fa42581dd259522659a0c55fc849c1289ec307
Jamie Sammons <jamie.sammons@liferay.com>
2025-04-28 22:46:32 -0500

    lazyvim

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 rm -f ~/.zshrc
 
-stow -t ~ nvim
 stow -t ~ starship
 stow -t ~ tmux
 stow -t ~ zshrc
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
 
 chsh -s $(which zsh)
